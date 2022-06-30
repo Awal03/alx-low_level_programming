e5213fa4b6c11f3cc8b2fde38146e2e6c6819f53
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-06-30 16:35:01 -0700

    Task 5 c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - prints the statement with proper grammar,
+* Return: 0 is an integer
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
