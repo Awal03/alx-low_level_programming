9eac042ed405e13bad85c039d1c23c16731a04e9
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-06-30 16:53:52 -0700

    Task 6 c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints size in byte
+ * Return: 0 is an integer
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
