380f7373ffb7370507756a6420ceed7ee57a7792
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-07-06 13:13:44 -0700

    Task 6 func

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,16 @@
 #include "main.h"
 
 /**
- * _abs - 
+ * _abs - Enters the absolute values
+ *
+ * @n: Computed integers
+ *
+ * Return: Integers absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
