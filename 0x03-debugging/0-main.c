bc05eea7d44be2d125f0b8d12e7bf6a326b023bb
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-07-07 03:05:30 -0700

    Task 0 bug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check if function prints whether integer is positive or negative
+ *
+ * Return: success always (0)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
