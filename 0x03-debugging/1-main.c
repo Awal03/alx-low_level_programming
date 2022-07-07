e67d02d47cca34b8102318e47340ae46406db9d6
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-07-07 03:15:40 -0700

    Task 1 bug

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - checks if it causes an infinite loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/* while (i < 10)*/
+	/* {*/
+	/* putchar(i);*/
+	/* }*/
+
+	ptintf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
