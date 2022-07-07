1fd1779e233eee30daba59955bd46cbf9fbcd059
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-07-07 14:02:42 -0700

    Task 2 more

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies wo integers
+ * @m: first integer
+ * @n: second integer
+ *
+ * Return: multiplicstion of m and n
+ */
+int mul(int m, int n)
+{
+	return (m * n);
+}
