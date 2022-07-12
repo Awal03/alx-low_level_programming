88e6e5b4466304fe9ea144fe1067292a76d9e0e5
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-07-12 07:09:52 -0700

    Task 2 pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,15 @@
 
 /**
  * _strlen - give the length of aa string
- * @s: the string 
+ * @s: the string
  *
  * Return: the length of a string.
  */
 int _strlen(char *s)
 {
 	int i;
-	for(i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
 	return (i);
 }
