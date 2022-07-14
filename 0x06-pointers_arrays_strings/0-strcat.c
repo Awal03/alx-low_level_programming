859ef9e9387f886d8ed66da51b191f8caa7c6cbc
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-07-14 06:54:54 -0700

    Task 0 m pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strcat - connects two strings
+ * @dest: destination integer
+ * @src: source integer
+ * Return: pointer to dest
+ */
+char   *_strcat(char *dest, char *src)
+{
+	int count = 0, count1 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count1 >= 0)
+	{
+		*(dest + count) = *(src + count1);
+		if (*(src + count1) == '\0')
+			break;
+			count++;
+			count1++;
+	}
+	return (dest);
+}
