6374f1ef55e68932bb6e81694b31b87ed39f3b3b
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-07-18 08:25:10 -0700

    Task 0 em pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-  *_memset - fills memory with a constant byte
-  *@s: memory area
-  *@n: number of byte to be filled
-  *@b: constant byte
-  *Return: return s
-  */
+ *_memset - fills memory with a constant byte
+ *@s: memory area
+ *@n: number of byte to be filled
+ *@b: constant byte
+ *Return: return s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
