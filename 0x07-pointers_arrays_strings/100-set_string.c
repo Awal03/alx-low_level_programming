254eff5dd597c506302be23caa488927bfbc1e5b
Awal03 <abdulrahamanadeyinka@gmail.com>
2022-07-18 14:57:00 -0700

    Task 8 em pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * set_string - set pointer value to char
+  * @s: source code
+  *@to: target code 
+  * Return: void
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
